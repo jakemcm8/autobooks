10c14b1f1808d896e2eae4323f263c81ca3fb77c
Jake <jakem80808@gmail.com>
2023-08-01 22:09:06 -0700

    style changes

--- src/app/components/profileDropdown.jsx
+++ src/app/components/profileDropdown.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function ProfileDropdown () {
   return (
     <div className="group text-right">
-      <Link href="/portfolio" className="bg-[#FFED66] text-[#FF5E5B] rounded-tl-lg rounded-b-lg opacity-80  py-1 px-2 top-0 mt-4 mr-8 right-0 text-xl z-10 fixed group-hover:border-2 group-hover:border-[#00CECB]">0x3ae...8c9</Link>
+      <Link href="/portfolio" className="bg-[#FFED66] text-[#FF5E5B] rounded-tl-lg rounded-b-lg opacity-80  py-1 px-2 top-0 mt-4 mr-8 right-0 text-xl z-10 fixed border-2 border-[#00CECB]">0x3ae...8c9</Link>
       <div className="hidden group-hover:block bg-[#FFED66] border-2 border-[#00CECB] p-8 rounded-tl-lg rounded-b-lg absolute w-64 right-0 top-0 mt-[17px] mr-[32px] z-5">
         <div className="relative mt-4 grid gap-2">
           <div>
--- src/app/current_markets/page.js
+++ src/app/current_markets/page.js
@@ -1,10 +1,25 @@
 import Link from 'next/link'
+import ProfileDropdown from '../components/profileDropdown.jsx'
 
 export default function Page () {
   return (
-    <div className='bg-[#FFFFEA] fixed bg-cover overflow-auto h-full w-full'>
-      <Link href=".."className="text-5xl font-extrabold text-[#FF5E5B] flex justify-start pl-5 pt-3">Betty</Link>
-      <h1>Markets</h1>
+    <div className='bg-[#FFFFEA] fixed flex bg-cover overflow-auto h-full w-full'>
+      <div className="">
+        <Link href=".."className="absolute text-5xl font-extrabold text-[#FF5E5B] justify-start pl-5 pt-3">Betty</Link>
+      </div>
+      <div className="flex flex-col items-center justify-center h-screen w-screen">
+        <h1 className="py-12">Markets</h1>
+        <div className="bg-[#FFED66] w-[42rem] h-[36rem] rounded-lg border-2 border-[#00CECB]">
+          <div>
+            <form >
+              <input className="border-2 border-[#00CECB] focus:bg-[ff5e5b]"></input>
+            </form>
+          </div>
+        </div>
+      </div>
+      <ProfileDropdown />
+
+
     </div>
 
   )
